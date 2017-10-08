6b214d4be3d0b91c1c593665aafd1f80b00c4b8f
mortezaho.1376@gmail.com <mortezaho.1376@gmail.com>
2017-10-08 18:12:38 +0330

    patrols but in wrong direction

--- main.cpp
+++ main.cpp
@@ -10,11 +10,17 @@
 #include <termios.h> /* POSIX terminal control definitions */
 #include <string.h>
 #include <stdlib.h>
+#include <cmath>
 
 using namespace std;
 using namespace cv;
 
 int fd;
+double degree = 0;
+double getSpeed(double dist)
+{
+	return ((dist / 100.0)*10.0) + 1;
+}
 int open_port(void)
 {
   int fd; /* File descriptor for the port */
@@ -34,14 +40,11 @@ int open_port(void)
 
   return (fd);
 }
-void init() 
-{
-	fd = open_port();
-	cv::namedWindow( "main", cv::WINDOW_AUTOSIZE );
-}
 int rotate_camera(int delta)
 {
 	//int fd = open_port();
+	if (!((degree + delta < 0) || (degree + delta > 180)))
+		degree += delta;
 	char s[13];
 	sprintf(s, "%d", delta);
 	strcat(s, "\n");
@@ -50,12 +53,20 @@ int rotate_camera(int delta)
 	  fputs("write() of 4 bytes failed!\n", stderr);
 	return 0;
 }
-
+void init() 
+{
+	for (int i = 0; i < 180; ++i)
+	{
+		rotate_camera(-1);
+	}
+	fd = open_port();
+	cv::namedWindow( "main", cv::WINDOW_AUTOSIZE );
+}
 
 int main( int argc, char** argv ) 
 {
 	init();
-
+	degree = 0;
 	// cv::namedWindow( "gaussian blur", cv::WINDOW_AUTOSIZE );
 	// cv::namedWindow( "hsv", cv::WINDOW_AUTOSIZE );
 	// cv::namedWindow( "inRange", cv::WINDOW_AUTOSIZE );
@@ -75,9 +86,12 @@ int main( int argc, char** argv )
  	Scalar color = Scalar( 0, 0, 255);
 	
  	vector<Point2f> movingPath;
+ 	Point2f lastDetect(1, 0);
+ 	bool founded = false;
+
 	while(true) 
 	{
-
+		founded = false;
 		g_cap >> frame;
 		w = frame.cols;
 		h = frame.rows; 
@@ -104,6 +118,8 @@ int main( int argc, char** argv )
 		vector< vector<Point> > contours;
 		findContours(dilated, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE);
 
+
+		
 		// you could also reuse img1 here
 		Mat contoursDrawed = Mat::zeros(dilated.rows, dilated.cols, CV_8UC1);
 
@@ -115,6 +131,9 @@ int main( int argc, char** argv )
 				mainConts.push_back(contours[i]);
 			}
 		}
+
+
+
 		drawContours(contoursDrawed, mainConts, -1, Scalar(255), CV_FILLED);
 
 		vector<vector<Point> > contours_poly( mainConts.size() );
@@ -128,16 +147,38 @@ int main( int argc, char** argv )
 			minEnclosingCircle( contours_poly[i], center[i], radius[i] );
 			circle( frame, center[i], (int)radius[i], color, 2, 8, 0 );
 			movingPath.push_back(center[i]);
+			lastDetect = center[i];
+			founded = true;
 
 
 			double rotated = (w / 2) - center[0].x;
 
 			double thresh = 20;
-			if (rotated < -1 * thresh)
+			//double speed = getSpeed(abs(rotated));
+			//cout << rotated << " ***** " << speed << endl;
+			cout << center[i].x << endl;
+			if (rotated < -1 * thresh){
 				rotate_camera(-1);
-			if (rotated > thresh)
+				cout<<"rotate left";
+			}
+			if (rotated > thresh){
 				rotate_camera(1);
+				cout<<"rotate right";
+			}
 				
+		}
+		int turn = 5;
+		if (!founded) {
+			if ((degree > 180 - turn || degree < turn)){
+				// turn = -1 * turn;
+				lastDetect.x = lastDetect.x * -1;
+			}
+			if(lastDetect.x > 0){
+				rotate_camera(turn);
+			}
+			else if(lastDetect.x < 0){
+				rotate_camera(-1 * turn);
+			}
 		}
 		if (movingPath.size() > 2)
 			for (int i = 0; i < movingPath.size() - 1; ++i)
@@ -148,8 +189,10 @@ int main( int argc, char** argv )
     	
     	while(movingPath.size() > 10)
     		movingPath.erase(movingPath.begin());
-
+    	
 		cv::imshow( "main", frame );
+
+
 		// cv::imshow( "gaussian blur", gaussianBlurred );
 		// cv::imshow( "hsv", HSVframe );
 		// cv::imshow( "inRange", inRangeOfGreen );
